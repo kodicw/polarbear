b777d27d4debdab94a1805d8046384d41101fc6a
kodicw <kodicw@users.noreply.github.com>
2026-06-27 22:26:00 -0700

    feat: add kodicw@penguin SSH key to root and charles authorized keys

--- modules/users/charles/authorized_keys.nix
+++ modules/users/charles/authorized_keys.nix
@@ -2,5 +2,5 @@
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICiDe2CMnDgWjXqMpQHxCSOmrjuAWwZazYPORZXlr2SF u0_a518@localhost"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFv8kP8LWCSGweBHEl/AmxKMLat2Y2RWonKqDSO6RrWb charles@mainframe"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIELV1RjFZ4PtSzmdtoKjbagU9H7YOYO/tTpepGLVndi droid@localhos”"
-
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID5nWlbj3PeD4/JQ8vwIIvdhab/geX9cUnJsMTMi1//T kodicw@penguin"
 ]
--- modules/users/root/default.nix
+++ modules/users/root/default.nix
@@ -3,6 +3,7 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFv8kP8LWCSGweBHEl/AmxKMLat2Y2RWonKqDSO6RrWb charles@mainframe"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEcgbJ626wZZs5aE9kbIc97LhDz9yeb/MmRPnYybpwJK root@mainframe"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID5nWlbj3PeD4/JQ8vwIIvdhab/geX9cUnJsMTMi1//T kodicw@penguin"
   ];
 }
 
